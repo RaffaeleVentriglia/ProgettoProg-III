ca697d13a2931e13263c4ff5ca9b4a69f7431df5
Raffaele Ventriglia <raffaeleventriglia6@gmail.com>
2023-02-10 13:09:45 +0100

    minor update

--- src/main/java/com/project/game/model/algorithm/ManhattanComparator.java
+++ src/main/java/com/project/game/model/algorithm/ManhattanComparator.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 public class ManhattanComparator implements Comparator<BoardPrototype> {
     @Override
     public int compare(BoardPrototype board1, BoardPrototype board2) {
-        int f1 = board1.getManhattanDistance();
-        int f2 = board2.getManhattanDistance();
+        int f1 = board1.getManhattanDistance() + board1.getG_n();
+        int f2 = board2.getManhattanDistance() + board2.getG_n();
         // Integer.compare ritorna:
         //          -1 se f1 è minore di f2
         //           1 se f1 è maggiore di f2
